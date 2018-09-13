95b50bd546665eadd7e0dd003d8611d19b37f66c
Max Sichel <msik314@gmail.com>
2018-09-12 23:58:56 -0400

    Add teleport functionality

--- Assets/Scripts/GridManager.cs
+++ Assets/Scripts/GridManager.cs
@@ -13,8 +13,14 @@ public class GridManager : MonoBehaviour
     private List<GameObject> pellets;
     private Vector2Int size;
     private Vector2Int offset;
+    
+    private List<Vector2Int>bluePos;
+    private List<Vector2Int>orangePos;
+    
     void Awake()
     {
+        bluePos = new List<Vector2Int>();
+        orangePos = new List<Vector2Int>();
         size = new Vector2Int();
         List<string> rows = new List<string>();
         StreamReader sr = new StreamReader(Application.dataPath + "/" + mapFile);
@@ -29,6 +35,8 @@ public class GridManager : MonoBehaviour
             line = sr.ReadLine();
         }
         
+        rows.Reverse();
+        
         size.y = rows.Count;
         
         offset = new Vector2Int(size.x/2, size.y/2);
@@ -42,7 +50,17 @@ public class GridManager : MonoBehaviour
             {
                 byte val = translateChar(rows[i][j]);
                 grid[i][j] = val;
-                Vector3 ip = new Vector3(j + offset.x, size.y - 1 - i + offset.y, 0);
+                if(val == 3)
+                {
+                    bluePos.Add(new Vector2Int(j, i));
+                }
+                
+                else if(val == 4)
+                {
+                    orangePos.Add(new Vector2Int(j, i));
+                }
+                
+                Vector3 ip = new Vector3(j + offset.x, i + offset.y, 0);
                 Instantiate(lookup[val], ip, Quaternion.identity, transform);
                 
             }
@@ -55,7 +73,48 @@ public class GridManager : MonoBehaviour
     
     public bool walk(Vector2Int last, Vector2Int next)
     {
-        return get(next.x, next.y) == 1;
+        Vector2Int index = next - offset;
+        return get(index.x, index.y) == 1;
+    }
+    
+    public bool eat(Vector2Int position)
+    {
+        Vector2Int index = position - offset;
+        byte val =  getVal(index.x, index.y);
+        if(val == 1 || val == 2)
+        {
+            set(index.x, index.y, 0);
+        }
+        
+        return val == 1 || val == 2;
+    }
+    
+    public Vector2Int teleport(Vector2Int position)
+    {
+        Vector2Int index = position - offset;
+        byte val = getVal(index.x, index.y);
+        if(val == 3)
+        {
+            for(int i = 0; i < bluePos.Count; ++i)
+            {
+                if(bluePos[i] == position)
+                {
+                    return orangePos[i] + offset;
+                }
+            }
+        }
+        else if(val == 4)
+        {
+            for(int i = 0; i < orangePos.Count; ++i)
+            {
+                if(orangePos[i] == position)
+                {
+                    return bluePos[i] + offset;
+                }
+            }
+        }
+        
+        return position;
     }
     
     public bool isIntersection(Vector2Int pos)
@@ -83,10 +142,27 @@ public class GridManager : MonoBehaviour
     {
         if(x > 0 && x < size.x && y > 0 && y < size.y)
         {
-            return (byte)(grid[y][x] < 6 ? 1:0);
+            return (byte)(grid[y][x] < 5 ? 1:0);
         }
         
         return 1;
+    }
+    
+    private byte getVal(int x, int y)
+    {
+        if(x > 0 && x < size.x && y > 0 && y < size.y)
+        {
+            return grid[y][x];
+        }
+        return 0;
+    }
+    
+    private void set(int x, int y, byte val)
+    {
+        if(x > 0 && x < size.x && y > 0 && y < size.y)
+        {
+             grid[y][x] = val;
+        }
     }
     
     private byte translateChar(char value)
